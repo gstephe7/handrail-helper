d0a7c949efcd1f234bcf49df31b1e309539588ca
Greg <gstephe7@gmail.com>
2018-07-21 09:29:36 -0400

    initial commmit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+const results = document.getElementById('results');
+const calculate = document.getElementById('calculate');
+const reset = document.getElementById('reset');
+const error = document.getElementById('error');
+var inches = document.getElementById('inches');
+var numerator = document.getElementById('numerator');
+var denominator = document.getElementById('denominator');
+var run = document.getElementById('run');
+var inchesValue = 0;
+var numeratorValue = 0;
+var denominatorValue = 0;
+var runValue = 0;
+var pitch = document.getElementById('pitch');
+var topRailRightValue = document.querySelectorAll('.topRailRightValue');
+var slopedPostValue = document.querySelectorAll('.slopedPostValue');
+var slopedRailRightValue = document.querySelectorAll('.slopedRailRightValue');
+
+
+function displayResults() {
+
+  inchesValue = Number(inches.value);
+  numeratorValue = Number(numerator.value);
+  denominatorValue = Number(denominator.value);
+  var rise = 0;
+
+  if (inchesValue > 0 || numeratorValue > 0) {
+
+    inches.style.outline = 'none';
+    numerator.style.outline = 'none';
+    denominator.style.outline = 'none';
+    error.style.display = 'none';
+
+    if (numeratorValue <= 0 || denominatorValue <= 0) {
+      rise = inchesValue;
+    } else {
+      rise = inchesValue + (numeratorValue / denominatorValue);
+    }
+
+    runValue = Number(run.value);
+
+    var angle = Math.atan(rise / runValue);
+    var degrees = Number((angle * 180 / Math.PI).toFixed(2));
+
+    pitch.innerHTML = degrees;
+
+    topRailRightValue.forEach(num => num.innerHTML = Number((180 - degrees) / 2).toFixed(1));
+    slopedPostValue.forEach(num => num.innerHTML = Number(90 - degrees).toFixed(1));
+    slopedRailRightValue.forEach(num => num.innerHTML = Number((90 + degrees) / 2).toFixed(1));
+
+    results.style.display = 'block';
+    results.scrollIntoView();
+
+  } else {
+
+    inches.style.outline = 'solid red';
+    numerator.style.outline = 'solid red';
+    denominator.style.outline = 'solid red';
+    error.style.display = 'block';
+
+  }
+
+}
+
+
+function resetResults() {
+  results.style.display = 'none';
+
+  inches.value = '';
+  numerator.value = '';
+  denominator.value = '';
+  run.value = 12;
+
+  inchesValue = 0;
+  numeratorValue = 0;
+  denominatorValue = 0;
+  runValue = 0;
+
+  inches.style.outline = 'none';
+  numerator.style.outline = 'none';
+  denominator.style.outline = 'none';
+  error.style.display = 'none';
+}
+
+
+calculate.addEventListener('click', displayResults);
+reset.addEventListener('click', resetResults);
